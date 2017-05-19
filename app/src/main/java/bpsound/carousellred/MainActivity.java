16b4b5575537fcb58cc493a4c44ca28e7842672b
elegantuniv <korea651@gmail.com>
2017-05-19 10:31:29 +0900

    binding view with butterknife

--- app/src/main/java/bpsound/carousellred/MainActivity.java
+++ app/src/main/java/bpsound/carousellred/MainActivity.java
@@ -2,12 +2,19 @@ package bpsound.carousellred;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.llMain) ListView mLvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
